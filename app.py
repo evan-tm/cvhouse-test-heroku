4973f54ea54fadfc36745e3fca3f1c8f04d47b25
Xinlun Cheng <chengxinlun@gmail.com>
2021-10-01 13:46:34 -0400

    Added year selection
    Added data table

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ import pandas as pd
 import geopandas as gpd
 import plotly.express as px
 import plotly.io as pio
+import plotly.graph_objects as go
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 
 
 mapbox_token_public = "pk.eyJ1IjoieGlubHVuY2hlbmciLCJhIjoiY2t0c3g2eHRrMWp3MTJ3cDMwdDAyYnA2OSJ9.tCcD-LyXD1OK-T6uDd8CYA"
@@ -14,35 +16,78 @@ mapbox_style = "mapbox://styles/xinluncheng/cktsxjvd923p618mw4fut9gav"
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 
-source = '''Data from Charlotteville Open Data Portal. Last update Sept 24,
-2021.'''
+source = '''Data from Charlotteville Open Data Portal. Last update Oct 1, 2021.
+Price has been adjusted for inflation.'''
 
-sales_2020 = gpd.read_file("sales_2020.geojson")
-vmin, vmax = np.nanpercentile(sales_2020["SaleAmount"], [5, 95])
-fig = px.choropleth_mapbox(sales_2020, geojson=sales_2020.geometry, 
-                           locations=sales_2020.index, color="SaleAmount",
-                           hover_name="Address", 
-                           center={"lat": 38.0293, "lon": -78.4767}, zoom=11, 
-                           width=700, height=700, range_color=[vmin, vmax], 
-                           labels={"SaleAmount": "Total Price", 
-                                   "PricePerSqft": "USD per Sqft", 
-                                   "SaleDate": "Date"},
-                           hover_data={"SaleAmount": True, "Acreage": True, 
-                                       "PricePerSqft": ":.2f", 
-                                       "SaleDate": True, "Zoning": True})
-fig.update_layout(mapbox_accesstoken=mapbox_token_public, 
-                  mapbox_style=mapbox_style)
+sales_clean_simple = gpd.read_file("real_estate_sales_simple.geojson")
+year = np.arange(1945, 2021, 1, dtype=int)
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 app.layout = html.Div(
     [
-        html.H1("Charlottesville Real Estate Sale in Year 2020"),
-        dcc.Graph(figure=fig),
-        dcc.Markdown(children=source)
+        html.H1("Charlottesville Real Estate Sale"),
+        html.Div([
+            dcc.Dropdown(id='year', options=[{'label': i, 'value': i} for i in year], value=2019),
+        ]),
+        html.Div([
+            dcc.Graph(id="graph"),
+        ]),
+        html.Div([
+            dcc.Graph(id="table"),
+        ]),
+        html.Div([
+            dcc.Markdown(children=source),
+        ]),
     ]
 )
+@app.callback(Output(component_id="graph",component_property="figure"), 
+              [Input(component_id='year',component_property="value"),])
+def change_year(y):
+    mask = (sales_clean_simple["Year"] == y)
+    sales_year = sales_clean_simple[mask]
+    vmin, vmax = np.nanpercentile(sales_year["SaleAmountAdjusted"], (5, 95))
+    fig = px.choropleth_mapbox(sales_year, 
+                               geojson=sales_year.geometry, 
+                               locations=sales_year.index, 
+                               color="SaleAmountAdjusted",
+                               range_color=[vmin, vmax],
+                               labels={"SaleAmountAdjusted": "Total Sale Price"},
+                               hover_name="Address", 
+                               hover_data={"SaleAmountAdjusted": True, 
+                                           "Acreage": True, 
+                                           "PricePerSqftAdjusted": ":.2f", 
+                                           "SaleDateStr": True, 
+                                           "Zone": True},
+                               center={"lat": 38.0293, "lon": -78.4767}, 
+                               zoom=11, 
+                               )
+    fig.update_layout(mapbox_accesstoken=mapbox_token_public, 
+                      mapbox_style=mapbox_style)
+    fig.update_traces(hovertemplate="<br>".join([
+        "Total Sale Price: $%{customdata[0]:,}",
+            "Acreage: %{customdata[1]:.3f}",
+            "Price per Sqft: $%{customdata[2]:.2f}",
+            "Last sale on %{customdata[3]}",
+            "Zoning: %{customdata[4]}"]))
+    return fig
+
+@app.callback(Output(component_id="table", component_property="figure"),
+              [Input(component_id='graph',component_property="clickData"),])
+def change_table(click):
+    if click is None:
+        return go.Figure()
+    addr = click['points'][0]['hovertext']
+    data = click['points'][0]['customdata']
+    fig = go.Figure(data=go.Table(header=dict(values=["Address", addr]),
+                                  cells=dict(values=[["Total Sale Price",
+                                                      "Acreage",
+                                                      "Pricer per Sqft",
+                                                      "Last sale",
+                                                      "Zoning"],
+                                                      data])))
+    return fig
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(host='127.0.0.1', debug=True)
