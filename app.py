1aec0bc657ba7ec3de1b8786a90cd0a42c9d1a98
Xinlun Cheng <chengxinlun@gmail.com>
2022-02-07 11:45:54 -0500

    Removed outdated components

--- app.py
+++ app.py
@@ -118,8 +118,9 @@ afford_prediction_algo = "Describe algorithm here. Some machine learning magic a
 imap_title = "Explore Charlottesville Housing Sales History"
 imap_sales_title = "Individual Sales"
 imap_neigh_title = "Neighborhood Averages"
+## Census
 census_title = "Census Information"
-source = '''Data from Charlotteville Open Data Portal. Last update Oct 28, 2021.
+source = '''Data from Charlotteville Open Data Portal. Last update Feb 7, 2022.
 Price has been adjusted for inflation. Only sales with state code Residential 
 (urban/suburban) and Multifamily are included.'''
 # ----------------------------------------------------------------------------
@@ -224,33 +225,10 @@ app.layout = html.Div(
                 # Algorithm description
                 html.Div([dcc.Markdown(afford_prediction_algo)], className="left_text bodytext")
             ], className="subcontainer"),
-        # Interactive map
-        html.Div(
-            [
-                html.Span(imap_title, className="center_text title", id="imap_title"),
-                html.Div(
-                [
-                    # Sales map
-                    html.Div(
-                        [
-                            html.Span(imap_sales_title, className="center_text subtitle"),
-                            dcc.Graph(id="sales_graph"),
-                        ], className="subcontainer background", style={"width": "100%"}),
-                    # Year Dropdown
-                    html.Div(
-                        [
-                            dcc.Dropdown(id='year', options=[{'label': i, 'value': i} for i in year], value=2019, 
-                                         style={"width": "200px"}),
-                            dcc.Graph(id="table", className="background"),
-                        ], className="subcontainer background", style={"width": "100%"}),
-                    # Average map
-                    html.Div(
-                        [
-                            html.Span(imap_neigh_title, className="center_text subtitle"),
-                            dcc.Graph(id="neigh_graph"),
-                        ], className="subcontainer background", style={"width": "100%"}),
-                ], className="grid_container", style={"grid-template-columns": "minmax(400px, 2fr) 1fr minmax(400px, 2fr)"}),
-            ], className="subcontainer"),
+        # Industry breakdown
+        # Neighborhood characteristics
+        # History of price
+        # Census
         html.Div(
             [
                 html.Span(census_title, id="census_title", className="center_text title"),
@@ -329,110 +307,6 @@ def update_afford_map(n, y, lod):
                       autosize=True,
                       font=dict(size=16, color="rgb(255,255,255)"))
     fig.update_yaxes(scaleanchor="x", scaleratio=1)
-    return fig
-
-# Individual sales year change
-@app.callback(Output(component_id="sales_graph", component_property="figure"), 
-              [Input(component_id='year', component_property="value")])
-def change_year_sales(y):
-    mask = (sales_clean_simple["Year"] == y)
-    sales_year = sales_clean_simple[mask].set_index("ParcelNumber")
-    vmin, vmax = np.nanpercentile(sales_year["SaleAmountAdjusted"], (5, 95))
-    fig = px.choropleth_mapbox(sales_year, 
-                               geojson=sales_year.geometry, 
-                               locations=sales_year.index, 
-                               color="SaleAmountAdjusted",
-                               range_color=[vmin, vmax],
-                               labels={"SaleAmountAdjusted": "Total Sale Price"},
-                               hover_name="Address", 
-                               hover_data={"SaleAmountStr": True, "AcreageStr": True, 
-                                           "SaleDateStr": True, "Zone": True},
-                               center={"lat": 38.0293, "lon": -78.4767}, 
-                               zoom=14,
-                              )
-    fig.update_traces(hovertemplate="<br>".join([
-        "%{hovertext}",
-        "",
-        "Total Sale Price: $%{customdata[0]}",
-        "Acreage: %{customdata[1]}",
-        "Last sale on %{customdata[2]}",
-        "Zoning: %{customdata[3]}"]))
-    fig.update_layout(mapbox_accesstoken=mapbox_token_public, 
-                      mapbox_style=mapbox_style,
-                      margin=go.layout.Margin(l=0, r=0,  b=0, t=0),
-                      plot_bgcolor="rgba(0,0,0,0)",
-                      paper_bgcolor="rgba(0,0,0,0)",
-                      autosize=True,
-                      font=dict(size=16, color="rgb(255,255,255)"))
-    fig.update_yaxes(scaleanchor="x", scaleratio=1)
-    return fig
-
-# Neighborhood average year change
-@app.callback(Output(component_id="neigh_graph", component_property="figure"), 
-              [Input(component_id='year', component_property="value")])
-def change_year_neigh(y):
-    mask = (sales_clean_simple["Year"] == y)
-    sales_year = sales_clean_simple[mask].set_index("ParcelNumber")
-    sales_year_nba = sales_year.groupby("Neighborhood").agg({"SaleAmountAdjusted": ["size", "mean"]}).reset_index()
-    sales_year_nba.columns = ["Neighborhood", "NumSales", "MeanSales"]
-    sales_year_nba = neighborhood_simple.merge(sales_year_nba, how="inner", right_on="Neighborhood", left_on="NAME")
-    sales_year_nba["MeanSalesStr"] = sales_year_nba["MeanSales"].apply(millify)
-    vmin, vmax = np.nanpercentile(sales_year_nba["MeanSales"], (5, 95))
-    fig = px.choropleth_mapbox(sales_year_nba, 
-                               geojson=sales_year_nba.geometry,
-                               locations=sales_year_nba.index, 
-                               color="MeanSales",
-                               range_color=[vmin, vmax],
-                               labels={"MeanSales": "Average Total Sale Price"},
-                               hover_name="Neighborhood", 
-                               hover_data={"MeanSalesStr": True, "NumSales": True},
-                               center={"lat": 38.0293, "lon": -78.4767}, 
-                               zoom=12, 
-                              )
-    fig.update_traces(hovertemplate="<br>".join([
-        "%{hovertext}",
-        "",
-        "Average Sale Price in Neighborhood: $%{customdata[0]}",
-        "Number of Sales Recorded: %{customdata[1]}"]))
-    fig.update_layout(mapbox_accesstoken=mapbox_token_public, 
-                      mapbox_style=mapbox_style,
-                      margin=go.layout.Margin(l=0, r=0,  b=0, t=0),
-                      plot_bgcolor="rgba(0,0,0,0)",
-                      paper_bgcolor="rgba(0,0,0,0)",
-                      autosize=True,
-                      font=dict(size=16, color="rgb(255,255,255)"))
-    fig.update_yaxes(scaleanchor="x", scaleratio=1)
-    return fig
-
-# Clicking on the interactive plots
-@app.callback(Output(component_id="table", component_property="figure"),
-              [Input(component_id='sales_graph',component_property="clickData"),
-               Input(component_id='neigh_graph',component_property="clickData"),])
-def change_table(click_s, click_n):
-    if (click_s is None) and (click_n is None):
-        fig = go.Figure(data=go.Table())
-    else:
-        ctx = dash.callback_context
-        dropdown_id = ctx.triggered[0]['prop_id'].split('.')[0]
-        if dropdown_id == "sales_graph":
-            # Individual sales
-            addr = click_s['points'][0]['hovertext']
-            data = click_s['points'][0]['customdata']
-            fig = go.Figure(data=go.Table(header=dict(values=["Address", addr]),
-                                          cells=dict(values=[["Total Sale Price", "Acreage", "Last sale on", "Zoning"],
-                                                             data])))
-        else:
-            # Neighborhood average
-            addr = click_n['points'][0]['hovertext']
-            data = click_n['points'][0]['customdata']
-            fig = go.Figure(data=go.Table(header=dict(values=["Neighborhood", addr]),
-                                          cells=dict(values=[["Average Sale Price", "Number of Sales"],
-                                                             data])))
-    fig.update_layout(margin=go.layout.Margin(l=0, r=0,  b=0, t=0),
-                      plot_bgcolor="rgba(0,0,0,0)",
-                      paper_bgcolor="rgba(0,0,0,0)",
-                      autosize=True,)
-    fig.update_traces(cells_font_size=15)
     return fig
 
 
