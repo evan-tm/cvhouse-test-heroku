7308dd59c03035091dcc30ac167c9385ba4aff15
evan-tm <evanmitchell@vt.edu>
2022-03-04 00:04:53 -0500

    hotfix

---
+++ library/cvillepedia.py
@@ -0,0 +1,36 @@
+
+dictionary = {
+    "Barracks Road": , 
+    "Rose Hill": , 
+    "Lewis Mountain": , 
+    "Starr Hill": ,
+    "Woolen Mills": , 
+    "10th & Page": "10th & Page is a Charlottesville neighborhood named for a \
+        street intersection near the center of the neighborhood. The 10th and Page \
+        neighborhood is one of Charlottesville's most compact neighborhoods, \
+        consisting of 83.86 acres. Much of the neighborhood was developed by \
+        John West, an emancipated slave who purchased land and subdivided it \
+        for African American families in the late 19th and early 20th century. \
+        The neighborhood was kept predominantly Black throughout the 20th century \
+        due to the work of people like Charles H. Brown who sought to ensure \
+        ownership stayed within African American families. The City worked \
+        with the Piedmont Housing Alliance to purchase thirteen single family \
+        homes for renovation. Some of these properties have been resold to \
+        individual families in the hopes of increasing a family-oriented neighborhood.", 
+    "The Meadows": , 
+    "Martha Jefferson": ,
+    "Johnson Village": , 
+    "Greenbrier": , 
+    "Barracks / Rugby": "The Barracks/Rugby Neighborhood is a combination of \
+        the smaller neighborhoods Rugby, Greenleaf, Kellytown, and Barracks \
+        Road. Each has a slightly different housing types. These areas became \
+        part of the City of Charlottesville between 1916 and 1963.", 
+    "North Downtown": ,
+    "Locust Grove": , 
+    "Jefferson Park Avenue": , 
+    "Fifeville": , 
+    "Fry's Spring": ,
+    "Ridge Street": , 
+    "Venable": , 
+    "Belmont": 
+}
