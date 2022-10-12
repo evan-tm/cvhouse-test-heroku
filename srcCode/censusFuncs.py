20ae1895de77241b406ca48a140784c59a0e7f12
Evan Mitchell <evan@Evans-MBP.local>
2022-10-12 11:59:15 -0400

    Population plot height update

--- srcCode/censusFuncs.py
+++ srcCode/censusFuncs.py
@@ -940,7 +940,7 @@ def plotSizeNeighborhood(n):
                         title="Population by Year for " + dfPopHood['NAME'][0],
                         labels={"Year": cd.text['SIZE_X_TITLE'],
                                 "Population": cd.text['SIZE_Y_TITLE']},
-                        markers = True, height = 500,
+                        markers = True, height = 450,
                         color_discrete_sequence=["#7c4375"])
         fig.update_layout(title_x=0.517)
 
